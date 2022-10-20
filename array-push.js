cb4e163b640d056dd3f7f8c4942799f88ba838f5
supriyashelke25 <supriyashelke683@gmail.com>
2022-10-20 10:15:01 +0530

    push file added

---
+++ array-push.js
@@ -0,0 +1,12 @@
+const arr = []
+arr.push("Supriya")
+arr.push("Sakshi")
+console.log(arr);
+
+arr.push("Mukesh")
+arr.push("Avishkar")
+console.log(arr);
+
+arr.push("Sakshi R")
+arr.push("Gayatri")
+console.log(arr);
